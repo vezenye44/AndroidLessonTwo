7aa25f2df075841a24cc668ee524f53d9772f8ac
Alecsey Efimov <alicsey2000@gmail.com>
2022-06-28 14:32:27 +0300

    Изменение главной активности(CalculateActivity). Добавление поля класса CalculatePresenter, отвечающего за логику приложения. Добавление OnClickListener всем кнопкам с цифрами.

--- app/src/main/java/ru/geekbrains/androidlessontwo/CalculateActivity.java
+++ app/src/main/java/ru/geekbrains/androidlessontwo/CalculateActivity.java
@@ -1,11 +1,16 @@
 package ru.geekbrains.androidlessontwo;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CalculateActivity extends AppCompatActivity implements CalculateView {
 
+    private CalculatePresenter presenter;
     private TextView resultView;
 
     @Override
@@ -14,6 +19,36 @@ public class CalculateActivity extends AppCompatActivity implements CalculateVie
         setContentView(R.layout.activity_calculate);
 
         resultView = findViewById(R.id.key_data_out);
+
+        Map<Integer, Integer> digits = new HashMap<>();
+        digits.put(R.id.key_0, 0);
+        digits.put(R.id.key_1, 1);
+        digits.put(R.id.key_2, 2);
+        digits.put(R.id.key_3, 3);
+        digits.put(R.id.key_4, 4);
+        digits.put(R.id.key_5, 5);
+        digits.put(R.id.key_6, 6);
+        digits.put(R.id.key_7, 7);
+        digits.put(R.id.key_8, 8);
+        digits.put(R.id.key_9, 9);
+
+        View.OnClickListener digitClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.onDigitClick(digits.get(view.getId()));
+            }
+        };
+
+        findViewById(R.id.key_0).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_1).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_2).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_3).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_4).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_5).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_6).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_7).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_8).setOnClickListener(digitClickListener);
+        findViewById(R.id.key_9).setOnClickListener(digitClickListener);
     }
 
     @Override
