e3de4c9164da11fad28d1edf710d7158386bf1d5
Alecsey Efimov <alicsey2000@gmail.com>
2022-06-28 17:47:24 +0300

    Создание интерфейса для модели, предназначенной выполнять операции сложения, вычитания, умножения и деление. Создание конкретной реализации для описанного выше интрфейса.

---
+++ app/src/main/java/ru/geekbrains/androidlessontwo/model/SimpleCalculate.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.androidlessontwo.model;
+
+import ru.geekbrains.androidlessontwo.Operate;
+
+public interface SimpleCalculate {
+    double calculation(double arg01, double arg02, Operate operate);
+}
