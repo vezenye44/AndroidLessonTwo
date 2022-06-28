9e33c2d5d17cd6140543e1ed6defa1a05ce61c81
Alecsey Efimov <alicsey2000@gmail.com>
2022-06-28 14:00:08 +0300

    Добавление вспомогательного интерфейса(CalculateView) для работы с выводом результата вычислений(единственный метод showResult()). Имплементация данного интерфейса основной активностью приложения-калькулятора.

--- app/src/main/java/ru/geekbrains/androidlessontwo/CalculateActivity.java
+++ app/src/main/java/ru/geekbrains/androidlessontwo/CalculateActivity.java
@@ -1,13 +1,23 @@
 package ru.geekbrains.androidlessontwo;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class CalculateActivity extends AppCompatActivity {
+public class CalculateActivity extends AppCompatActivity implements CalculateView {
+
+    private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate);
+
+        resultView = findViewById(R.id.key_data_out);
+    }
+
+    @Override
+    public void showResult(String result) {
+        resultView.setText(result);
     }
 }
