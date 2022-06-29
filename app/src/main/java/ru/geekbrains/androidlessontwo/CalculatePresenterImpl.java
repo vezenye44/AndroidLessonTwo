042dc22412c1d7519848b890a02cbcf86c1118a1
Alecsey Efimov <alicsey2000@gmail.com>
2022-06-29 07:21:44 +0300

    Создание класса обьектов(CalculatePresenterImpl) с данными и операциями калькулятора, без внутренней реализации.

---
+++ app/src/main/java/ru/geekbrains/androidlessontwo/CalculatePresenterImpl.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.androidlessontwo;
+
+import ru.geekbrains.androidlessontwo.model.SimpleCalculate;
+
+public class CalculatePresenterImpl implements CalculatePresenter {
+
+    /** Поле - реализация простого калькулятора */
+    private SimpleCalculate calculate;
+
+    /** Поле-интерфейс для вывода результатов вычислений во вне */
+    private CalculateView resultView;
+
+    /** Поле - последняя вызванная операция */
+    private Operate operate;
+
+    /** Публичный конструктор, получающий необходимые для работы обьекты
+     * @param calculate Реалмизация простого калькулятора, умеющая выполнять простые
+     * бинарные операции над действительными числами.
+     * @param resultView Реализация интерфейса для вывода результатов вычислений во вне.
+     */
+    public CalculatePresenterImpl(SimpleCalculate calculate, CalculateView resultView) {
+        this.calculate = calculate;
+        this.resultView = resultView;
+    }
+
+    /** Поле - первый аргумент */
+    private double arg01;
+    /** Поле - второй аргумент */
+    private double arg02;
+
+    @Override
+    public void onDigitClick(Integer digit) {
+
+    }
+
+    @Override
+    public void onDotClick() {
+
+    }
+
+    @Override
+    public void onOperateClick(Operate operate) {
+
+    }
+
+    @Override
+    public void onResetClick() {
+
+    }
+}
