242173f9f5d1cdf325ddb7a19879148196de14b3
Alecsey Efimov <alicsey2000@gmail.com>
2022-07-01 13:40:49 +0300

    Изменение интерфейса CalculatePresenter. Добавление метода onResume() для продолжения работы с калькулятором, после пересоздания активности.

--- app/src/main/java/ru/geekbrains/androidlessontwo/presenter/CalculatePresenter.java
+++ app/src/main/java/ru/geekbrains/androidlessontwo/presenter/CalculatePresenter.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.androidlessontwo.presenter;
 
 import ru.geekbrains.androidlessontwo.model.Operate;
+import ru.geekbrains.androidlessontwo.ui.CalculateView;
 
 public interface CalculatePresenter {
+
+    void onResume(CalculateView resultView);
 
     void onDigitClick(Integer digit);
 
