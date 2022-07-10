2c8bcff08060795cbf65b606e41341927ddd4554
Alecsey Efimov <alicsey2000@gmail.com>
2022-07-10 06:06:00 +0300

    Изменения в главной активности приложения. Внесение некоторых правок в код. Добавление реакции на нажатие на кнопку выбора тем с использованием  Activity Result API.

--- app/src/main/java/ru/geekbrains/androidlessontwo/ui/CalculateActivity.java
+++ app/src/main/java/ru/geekbrains/androidlessontwo/ui/CalculateActivity.java
@@ -1,29 +1,41 @@
 package ru.geekbrains.androidlessontwo.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import ru.geekbrains.androidlessontwo.model.SimpleCalculateImpl;
-import ru.geekbrains.androidlessontwo.presenter.CalculatePresenter;
-import ru.geekbrains.androidlessontwo.model.Operate;
 import ru.geekbrains.androidlessontwo.R;
+import ru.geekbrains.androidlessontwo.model.Operate;
+import ru.geekbrains.androidlessontwo.model.SimpleCalculateImpl;
+import ru.geekbrains.androidlessontwo.model.ThemeRepository;
+import ru.geekbrains.androidlessontwo.model.ThemeRepositoryImpl;
+import ru.geekbrains.androidlessontwo.presenter.CalculatePresenter;
 import ru.geekbrains.androidlessontwo.presenter.CalculatePresenterImpl;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CalculateActivity extends AppCompatActivity implements CalculateView {
 
     private static final String CALCULATE_PRESENTER = "presenter";
+
+    private ActivityResultLauncher<Intent> startSelectTheme;
     private CalculatePresenter presenter;
     private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ThemeRepository repository = ThemeRepositoryImpl.getInstance(this);
+        setTheme(repository.getSaveTheme().getThemeResource());
+
         setContentView(R.layout.activity_calculate);
 
         resultView = findViewById(R.id.key_data_out);
@@ -31,7 +43,7 @@ public class CalculateActivity extends AppCompatActivity implements CalculateVie
         if (savedInstanceState == null) {
             presenter = new CalculatePresenterImpl(new SimpleCalculateImpl(), CalculateActivity.this);
         } else {
-            presenter = (CalculatePresenterImpl) savedInstanceState.getParcelable(CALCULATE_PRESENTER);
+            presenter = savedInstanceState.getParcelable(CALCULATE_PRESENTER);
             presenter.onResume(new SimpleCalculateImpl(), this);
         }
 
@@ -102,6 +114,24 @@ public class CalculateActivity extends AppCompatActivity implements CalculateVie
             @Override
             public void onClick(View view) {
                 presenter.onResetClick();
+            }
+        });
+
+        startSelectTheme = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            CalculateActivity.this.recreate();
+                        }
+                    }
+                });
+
+        findViewById(R.id.key_select_theme).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSelectTheme.launch(new Intent(CalculateActivity.this, SelectThemeActivity.class));
             }
         });
 
